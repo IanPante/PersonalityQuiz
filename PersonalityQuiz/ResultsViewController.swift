e8a39b2e18af8d6c293e2f3f89fffbf5d10d6278
Ian <ianpante15@gmail.com>
2022-03-18 22:32:24 +0800

    Add Calculate and Display Result

--- PersonalityQuiz/ResultsViewController.swift
+++ PersonalityQuiz/ResultsViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    @IBOutlet var resultAnswerLabel: UILabel!
+    @IBOutlet var resultDefinitionLabel: UILabel!
+    
     var responses: [Answer]
 
     init?(coder: NSCoder, responses: [Answer]) {
@@ -22,10 +25,28 @@ class ResultsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        calculatePersonalityResult()
+        
+        navigationItem.hidesBackButton = true
     }
     
+    func calculatePersonalityResult() {
+        let frequencyOfAnswers = responses.reduce(into:
+           [AnimalType: Int]()) { (counts, answer) in
+            if let existingCount = counts[answer.type] {
+                counts[answer.type] = existingCount + 1
+            } else {
+                counts[answer.type] = 1
+            }
+        }
+    
+    let mostCommonAnswer = frequencyOfAnswers.sorted { $0.1 > $1.1 }.first!.key
+    
+    resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+    resultDefinitionLabel.text = mostCommonAnswer.definition
+    
+    }
 
     /*
     // MARK: - Navigation
